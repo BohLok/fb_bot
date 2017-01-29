e08f3d6132b6dbee3120eed847a753cffbbc16ca
Boh <bohlok@gmx.de>
2017-01-29 18:18:15 +0000

    send_text_message

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -1,3 +1,5 @@
+
+
 class BotController < ApplicationController
 
 
@@ -16,12 +18,31 @@ def receive_message
      messaging_events.each do |event|
      sender = event[:sender][:id]
      if (text = event[:message] && event[:message][:text])
-        send_text_message(sender, “Hi there! You said: #{text}. The Bots”)
+        send_text_message(sender, "Hi there! You said:. The Bots")
      end
    end
  end
 
  render nothing: true
 end
+
+def send_text_message
+body = {
+ recipient: {
+   id: sender
+ },
+ message: {
+   text: text
+ }
+}.to_json
+response = HTTParty.post(
+ "https://graph.facebook.com/v2.6/me/messages?access_token=#{EAASMK8x5ZBFcBAGtOJEqjHfk7cggpUVnDzLTUaw2wpinVkxzMZANRYfoCVdwZCiMzfx9L0D2fnpjl1SKg8up1gbO6ZC66j6TqAZByNgH8l5cylQqrb46t6He3IXyuQK9rBE7Qp2wDZBi1cMhNJvxsnS52aci7yUYTuF9bdqB3AEQZDZD}",
+ body: body,
+ headers: { 'Content-Type' => 'application/json' }
+)
+  
+end
+
+
 
 end
